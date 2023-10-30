625693a19a544dda45f23fb60be2bd3d374d22da
learner1204 <rroshanuik@gmail.com>
2023-10-31 04:12:33 +0530

    user interface done

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Petopia
-Dog adoption website using HTML, CSS, JS and FLASK
+
+<b><h1 style="text-align: center;">TinDog</h1></b>
+
+- [run using virtual environment](http://localhost:63342/TinDog/templates/index.html?_ijt=gutn5khs9pvkaaj2ldia2iuegl&_ij_reload=RELOAD_ON_SAVE)
+
+
+## Tech used
+* HTML
+* CSS
+* JavaScript
+* Bootstrap
+
---
+++ templates/script/script.js
@@ -0,0 +1,25 @@
+document.querySelector('.custom-toggler').addEventListener('click', function () {
+    const navbarTogglerIcon = document.getElementById('navbar-toggler');
+    if (this.getAttribute('aria-expanded') === 'false') {
+      navbarTogglerIcon.classList.remove('btn-close-white');
+      navbarTogglerIcon.classList.remove('btn-close');
+      navbarTogglerIcon.classList.add('navbar-toggler-icon'); 
+    } else {
+      navbarTogglerIcon.classList.add('btn-close-white');
+      navbarTogglerIcon.classList.add('btn-close');
+      navbarTogglerIcon.classList.remove('navbar-toggler-icon'); 
+    }
+  });
+  
+
+// Dark Theme
+let themeToggleBtn = document.querySelector('.toggle-theme');
+
+themeToggleBtn.addEventListener('click', () => {
+  if (document.body.classList.contains('dark')) {
+    document.body.classList.remove('dark');
+  } else {
+    document.body.classList.add('dark');
+  }
+  console.log('hello');
+});
